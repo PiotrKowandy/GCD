01326368b430995d6c5a2262bfde89c033d54c44
Piotr Kowańdy <phoennix@Phoennix-Mac.local>
2020-11-22 21:07:00 +0100

    build + google to ignore

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/phoennix/Desktop/GCD/googletest/googlemock
-# Build directory: /Users/phoennix/Desktop/GCD/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
